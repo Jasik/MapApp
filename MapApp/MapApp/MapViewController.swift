f783f6f4c4e4a0a7cb0d292f48e502ea17e5d766
Jasik <rogozhkin87@gmail.com>
2020-09-24 20:29:46 +0900

    ViewController to renamed

--- MapApp/MapApp/MapViewController.swift
+++ MapApp/MapApp/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  MapApp
 //
 //  Created by Vladimir Rogozhkin on 2020/09/24.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
